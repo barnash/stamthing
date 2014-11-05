418739ffe52f96b315269788c8543b3b3fb77ae0
Aviv Ben-Yosef <aviv.by@gmail.com>
2014-11-05 14:19:04 +0200

    Add stuff for Aviv

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function main() {
     var bodyEl = document.getElementsByTagName('body')[0];
     addIftachStuff(bodyEl);
+    addAvivStuff(bodyEl);
 };
 
 function addIftachStuff(bodyEl) {
     var d = document.createElement('h3');
     d.textContent = 'Iftach is the man';
     bodyEl.appendChild(d);
+}
+
+function addAvivStuff(bodyEl) {
+    var d = document.createElement('h1');
+    d.textContent = 'My name is Inigo Montoya';
+    bodyEl.insertBefore(d, bodyEl.firstChild);
 }
