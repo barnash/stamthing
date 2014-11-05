80b27a2f4a33d2bf7480275caa0343ae6400f2d1
Iftach Bar <iftachbar@gmail.com>
2014-11-05 14:13:37 +0200

    Added my part of the exercise

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function main() {
+    var bodyEl = document.getElementsByTagName('body')[0];
+    addIftachStuff(bodyEl);
 };
+
+function addIftachStuff(bodyEl) {
+    var d = document.createElement('h3');
+    d.textContent = 'Iftach is the man';
+    bodyEl.appendChild(d);
+}
