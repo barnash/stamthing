c183c429b768665a7d642aa7bf7a39aea40dbe24
Iftach Bar <iftachbar@gmail.com>
2014-11-05 14:28:28 +0200

    moved my things to a different file

---
+++ iftach.js
@@ -0,0 +1,5 @@
+function addIftachStuff(bodyEl) {
+    var d = document.createElement('h3');
+    d.textContent = 'Aviv Ben-Yosef was here before me';
+    bodyEl.appendChild(d);
+}
--- index.js
+++ index.js
@@ -4,12 +4,6 @@ function main() {
     addAvivStuff(bodyEl);
 };
 
-function addIftachStuff(bodyEl) {
-    var d = document.createElement('h3');
-    d.textContent = 'Aviv Ben-Yosef was here before me';
-    bodyEl.appendChild(d);
-}
-
 function addAvivStuff(bodyEl) {
     var d = document.createElement('h1');
     d.textContent = 'My name is Inigo Montoya (this was done after Iftach)';
