f317c2d7a922428d474dd0af3dd1017712382b3e
Aviv Ben-Yosef <aviv.by@gmail.com>
2014-11-05 14:27:37 +0200

    refactor out aviv code

---
+++ aviv.js
@@ -0,0 +1,5 @@
+function addAvivStuff(bodyEl) {
+    var d = document.createElement('h1');
+    d.textContent = 'My name is Inigo Montoya (this was done after Iftach)';
+    bodyEl.insertBefore(d, bodyEl.firstChild);
+}
--- index.js
+++ index.js
@@ -9,9 +9,3 @@ function addIftachStuff(bodyEl) {
     d.textContent = 'Aviv Ben-Yosef was here before me';
     bodyEl.appendChild(d);
 }
-
-function addAvivStuff(bodyEl) {
-    var d = document.createElement('h1');
-    d.textContent = 'My name is Inigo Montoya (this was done after Iftach)';
-    bodyEl.insertBefore(d, bodyEl.firstChild);
-}
